70ffc2338b2c9969cc9dddb8785565f412d8f0c2
Ben Franske <ben.mm@franske.com>
2025-01-29 20:54:52 -0600

    fix bad typos in server name

--- passport.py
+++ passport.py
@@ -23,8 +23,8 @@ except IOError:
 app = Flask(__name__)
 # Translate yaml config to flask config for session type
 app.config['SESSION_TYPE'] = passportConfig['sessionType']
-app.config['SERVER_NAME'] = passportConfig['apps.trolleyride.org']
-app.config['PREFERRED_URL_SCHEME'] = passportConfig['https']
+app.config['SERVER_NAME'] = passportConfig['serverName']
+app.config['PREFERRED_URL_SCHEME'] = 'https'
 
 Session(app)
 
